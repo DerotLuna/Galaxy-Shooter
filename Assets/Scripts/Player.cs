2e54ce1847be1f5eb2f6aa63a6732936f1dd8b23
lunaru <rubenlunamate@gmail.com>
2023-01-28 19:19:28 -0400

    GS-5 set start position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update (1 time)
     void Start()
     {
-        
+        //current pos = new position
+        transform.position = new Vector3(0f, 0f, 0f);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame. (After Start and 60 times per second )
     void Update()
     {
         
