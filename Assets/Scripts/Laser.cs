c5279c4c555dedf5ca2ba96f2bb873f0ad6780f3
Ruben Luna <rubenlunamate@gmail.com>
2023-01-29 21:26:38 -0400

    GS-9-power-up-triple-shot (#7)
    * GS-8 shooting (#6)
    
    * GS-13 laser configured
    
    * GS-14 instantiated laser
    
    * GS-15 laser behaviour configured: move and destroy
    
    * GS-16 cool down system configured. Others: f was changed to F in the floats
    
    * GS-8 the player is shooting
    
    * GS-17 triple shoot implemented
    
    * GS-20 collision detections implemented
    
    * GS-19 communication between script Powerup and Player
    
    * GS-18 in this case, implemented: Power down Routine (GS20) and GS-20 Collision detections. Error type Jira code :c

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laserMovementController();
+    }
+
+    private void laserMovementController()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 6.0F)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public bool canTripleShoot = false;
+
     [SerializeField]
-    private float speed = 5.0f;
+    private float _speed = 5.0F;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private GameObject _tripleLaserPrefab;
+
+    //fireRate is 0.25f
+    //canFire -- has the amount of time between firing passed? Time.time
+    [SerializeField]
+    private float _fireRate = 0.20F;
+
+    private float _nextFire = 0.0F;
 
     // Start is called before the first frame update (1 time)
     void Start()
     {
         //current pos = new position
-        transform.position = new Vector3( 0f, 0f, 0f );
+        transform.position = new Vector3(0F, 0F, 0F);
     }
 
     // Update is called once per frame. (After Start and 60 times per second )
     void Update()
     {
         playerMovementController();
+        shoot();
     }
 
     private void playerMovementController()
@@ -25,33 +41,79 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime);
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
 
         limits_x();
         limits_y();
     }
 
+    private void shoot()
+    {
+        //Space and right mouse click
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (Time.time > _nextFire)
+            {
+                if (canTripleShoot)
+                {
+                    //center laser, right laser, and left laser
+                    Instantiate(_tripleLaserPrefab,
+                    transform.position,
+                    Quaternion.identity);
+                }
+                else
+                {
+                    //center laser
+                    Instantiate(_laserPrefab,
+                    new Vector3(transform.position.x, transform.position.y + 0.85F, transform.position.z),
+                    Quaternion.identity);
+                }
+
+                _nextFire = Time.time + _fireRate;
+            }
+        }
+    }
+
     private void limits_y()
     {
-        if (transform.position.y > 0f)
+        if (transform.position.y > 0F)
         {
-            transform.position = new Vector3( transform.position.x, 0f, 0f );
+            transform.position = new Vector3(transform.position.x, 0F, 0F);
         }
-        else if(transform.position.y < -4.25f)
+        else if (transform.position.y < -4.25F)
         {
-            transform.position = new Vector3( transform.position.x, -4.25f, 0f );
+            transform.position = new Vector3(transform.position.x, -4.25F, 0F);
         }
     }
 
     private void limits_x()
     {
-        if (transform.position.x > 9.5f)
+        if (transform.position.x > 9.5F)
         {
-            transform.position = new Vector3(-9.5f, transform.position.y, 0f);
+            transform.position = new Vector3(-9.5F, transform.position.y, 0F);
         }
-        else if (transform.position.x < -9.5f)
+        else if (transform.position.x < -9.5F)
         {
-            transform.position = new Vector3(9.5f, transform.position.y, 0f);
+            transform.position = new Vector3(9.5F, transform.position.y, 0F);
         }
+    }
+
+    private void instantiateLaserPrefab(float x, float y)
+    {
+        Instantiate(_laserPrefab,
+                    new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z),
+                    Quaternion.identity);
+    }
+
+    private IEnumerator tripleShootPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(3);
+        canTripleShoot = false;
+    }
+
+    public void tripleShootPowerupOn()
+    {
+        canTripleShoot = true;
+        StartCoroutine(tripleShootPowerDownRoutine());
     }
 }
