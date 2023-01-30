d173a6c7a78f3eec88f5ae65f40d23289be57bd7
lunaru <rubenlunamate@gmail.com>
2023-01-29 20:06:17 -0400

    GS-20 collision detections implemented

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+    }
+}
